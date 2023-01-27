e26b93382a49eb69a9dd90b7fa47ca091ec0a636
gutierrer <11924623+regutierrez@users.noreply.github.com>
2023-01-27 21:30:31 +0800

    add selectors

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+const numBtn = document.querySelectorAll("[num]");
+const operatorBtn = document.querySelectorAll("[operator]");
+const equalsBtn = document.getElementById("equals");
+const clearBtn = document.getElementById("clear");
+const deleteBtn = document.getElementById("delete");
+const pointBtn = document.getElementById("point");
+const resultScreen = document.getElementById("result-screen");
+const inputScreen = document.getElementById("input-screen");
+
+window.addEventListener("keydown", handle);
+
 function add(x, y) {
   return x + y;
 }
-function subtracta(x, y) {
+function subtract(x, y) {
   return x - y;
 }
 function multiply(x, y) {

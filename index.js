4fb77db2bac5eadde7cd0bfda732972083c1d4e0
regutierrez <11924623+regutierrez@users.noreply.github.com>
2023-09-06 22:30:53 +0800

    progress...

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+let firstOperand = ''
+let secondOperand = ''
+let currentOperation = null
+let resetScreen = false
+
 const numBtn = document.querySelectorAll("[num]");
 const operatorBtn = document.querySelectorAll("[operator]");
 const equalsBtn = document.getElementById("equals");
@@ -13,8 +18,34 @@ numBtn.forEach((button) =>
   button.addEventListener("click", () => appendNumber(button.textContent))
 );
 
-function appendNumber(num) {
-  if (inputScreen.textContent === "0") resetScreen;
+clearBtn.addEventListener("click", () => clearScreen())
+
+function resetResultScreen() {
+  inputScreen.textContent = ''
+  resetScreen = false
+}
+
+function clearScreen() {
+  console.log('clear')
+  resultScreen.textContent = ''
+  inputScreen.textContent = '0'
+  firstOperand = ''
+  secondOperand = ''
+  currentOperation = null
+}
+
+function appendNumber(number) {
+  if (inputScreen.textContent === '0' || resetScreen){
+    console.log(resetScreen)
+    resetResultScreen()
+  }
+  inputScreen.textContent += number
+}
+
+function handleKeyStroke(e) {
+  if (e.key >=  0 && e.key <= 9){
+    appendNumber(e.key)
+  } 
 }
 
 function add(x, y) {
